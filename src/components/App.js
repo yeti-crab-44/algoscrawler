7cb2093db78687d95acb90cad59cceac23138598
Rose <rhoswen827@gmail.com>
2023-11-25 10:22:07 -0500

    m

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { store } from '../app/store';
 import { Provider } from 'react-redux';
-
 import '../stylesheets/styles.scss';
 
-// const App = () => {
-//   return <div>I am inside App.js!</div>;
-// };
-//previous 3 lines replaced by ReactDOM.render block
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+const App = () => {
+  return <div>I am inside App.js!</div>;
+};
 
 export default App;
