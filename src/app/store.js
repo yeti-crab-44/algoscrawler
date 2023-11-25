35e8aeb3137b843c78afe7cc28a17d6daae90688
kchandler44 <kellychandler624@gmail.com>
2023-11-24 17:40:42 -0700

    webpack created

---
+++ src/app/store.js
@@ -0,0 +1,7 @@
+//most of the text here copied from https://redux-toolkit.js.org/tutorials/quick-start
+
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {},
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { store } from '../app/store';
+import { Provider } from 'react-redux';
+
 import '../stylesheets/styles.scss';
 
-const App = () => {
-  return <div>I am inside App.js!</div>;
-};
+// const App = () => {
+//   return <div>I am inside App.js!</div>;
+// };
+//previous 3 lines replaced by ReactDOM.render block
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 export default App;
