5a45975be26b8d93c50bf57e9976319cc5b5e005
kchandler44 <kellychandler624@gmail.com>
2023-11-25 11:51:44 -0700

    errors created

--- server/server.js
+++ server/server.js
@@ -2,5 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(3000);
+// app.get
+
+app.use('*', (req, res) => {
+  res.status(404).send('Error 404!');
+});
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: "I'm sorry, I can't find the page you're looking for." },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
 module.exports = app;
