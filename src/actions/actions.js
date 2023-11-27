d4423f59dac25ae21fe000cef4b6ed04cf87b2b6
Rose <rhoswen827@gmail.com>
2023-11-26 20:32:44 -0500

    finish frontend GET_ALL_PROBLEMS

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,9 +1,25 @@
 import * as types from '../constants/actionTypes';
 
 // action creators
-export const viewAllProblems = () => ({
-  type: types.VIEW_ALL_PROBLEMS,
-});
+//thunk allows action creator to return a function other than an object
+export const viewAllProblems = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch('/api/problems', {
+        method: 'GET',
+      });
+
+      const data = await response.json();
+
+      dispatch({
+        type: VIEW_ALL_PROBLEMS,
+        payload: data,
+      });
+    } catch (error) {
+      console.error('Error fetching problems:', error);
+    }
+  };
+};
 
 export const addProblem = (problem) => ({
   type: types.ADD_PROBLEM,
